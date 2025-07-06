a1ce594c641152fc8f052fa43c852553e671d0a3
Devin Jameson <devinjameson@hey.com>
2025-07-06 16:31:24 -0400

    Fix DatePicker styling

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
+import 'react-day-picker/style.css'
+import './globals.css'
+
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
-import './globals.css'
 import { Providers } from '@/lib/providers'
 
 const inter = Inter({
--- src/ui/DatePicker.tsx
+++ src/ui/DatePicker.tsx
@@ -1,12 +1,14 @@
 'use client'
 
-import { DayPicker } from 'react-day-picker'
+import { DayPicker, getDefaultClassNames } from 'react-day-picker'
 import { DateTime } from 'effect'
 import { CalendarIcon } from '@heroicons/react/16/solid'
 import { Input } from '@/ui/catalyst/input'
 import { forwardRef } from 'react'
 import clsx from 'clsx'
 import { Popover, PopoverButton, PopoverPanel } from '@headlessui/react'
+
+const defaultClassNames = getDefaultClassNames()
 
 interface DatePickerProps {
   value?: Date
@@ -22,7 +24,7 @@ export const DatePicker = forwardRef<HTMLInputElement, DatePickerProps>(function
 ) {
   return (
     <Popover className="relative">
-      <PopoverButton as="div" className="relative">
+      <PopoverButton as="div" className="relative cursor-default">
         <Input
           ref={ref}
           name={name}
@@ -31,7 +33,7 @@ export const DatePicker = forwardRef<HTMLInputElement, DatePickerProps>(function
           }
           placeholder={placeholder}
           readOnly
-          className={clsx(className, 'pr-10')}
+          className={clsx(className, 'pointer-events-none')}
         />
         <CalendarIcon className="pointer-events-none absolute right-3 top-1/2 size-5 -translate-y-1/2 text-zinc-500" />
       </PopoverButton>
@@ -40,7 +42,7 @@ export const DatePicker = forwardRef<HTMLInputElement, DatePickerProps>(function
         anchor="bottom"
         transition
         className={clsx(
-          'isolate mt-2 w-max rounded-xl p-3 bg-white/75 backdrop-blur-xl dark:bg-zinc-800/75 shadow-lg ring-1 ring-zinc-950/10 dark:ring-white/10 dark:ring-inset transition data-closed:opacity-0 data-closed:translate-y-1',
+          'isolate mt-2 rounded-xl p-3 bg-white/75 backdrop-blur-xl dark:bg-zinc-800/75 shadow-lg ring-1 ring-zinc-950/10 dark:ring-white/10 dark:ring-inset transition data-closed:opacity-0 data-closed:translate-y-1',
         )}
       >
         {({ close }) => (
@@ -50,6 +52,9 @@ export const DatePicker = forwardRef<HTMLInputElement, DatePickerProps>(function
             onSelect={(date) => {
               onChange(date)
               close()
+            }}
+            classNames={{
+              day: `${defaultClassNames.day} rounded-full hover:bg-zinc-300`,
             }}
           />
         )}
